f9542d56bbd834778df748c0dd6ece9acc580bdb
disc99 <disc99.g@gmail.com>
2018-06-20 13:01:00 +0900

    Add maven publish

--- src/main/java/io/disc99/protocol/buffers/spring/cloud/stream/ProtocolBuffersSpringCloudStreamAutoConfiguration.java
+++ src/main/java/io/disc99/protocol/buffers/spring/cloud/stream/ProtocolBuffersSpringCloudStreamAutoConfiguration.java
@@ -1,5 +1,6 @@
 package io.disc99.protocol.buffers.spring.cloud.stream;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.stream.annotation.StreamMessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -7,9 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MessageConverter;
 
 @Configuration
+@ConditionalOnClass(StreamMessageConverter.class)
 public class ProtocolBuffersSpringCloudStreamAutoConfiguration {
     @Bean
-    @ConditionalOnMissingBean
     @StreamMessageConverter
     public MessageConverter protobufMessageConverter() {
         return new ProtobufMessageConverter();
